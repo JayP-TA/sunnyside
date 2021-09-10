b10378967e8769540f21b7853b06d3abd4b68fc3
Juan Pablo Torres <jptorressnl19@gmail.com>
2021-09-10 08:30:57 -0600

    Adds display toggle for responsive nav

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Script for responsive nav
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("hamburger");
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+})
